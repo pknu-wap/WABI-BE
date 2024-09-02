35fb7cb93cde8d82e2d0bd343f0f8a3b5764c213
FhRh <yiyop@naver.com>
2024-09-02 18:37:07 +0900

    fix : 리소스가 존재하지 않는 것은 404에러이다.

--- wabi/src/main/kotlin/com/wap/wabi/exception/ErrorCode.kt
+++ wabi/src/main/kotlin/com/wap/wabi/exception/ErrorCode.kt
@@ -1,9 +1,6 @@
 package com.wap.wabi.exception
 enum class ErrorCode (val code: String = "", val message: String = ""){
     BAD_REQUEST("400", "Invalid request."),
-    BAD_REQUEST_BAND("400-1", "밴드Id를 확인해주세요."),
-    BAD_REQUEST_EVENT("400-2", "이벤트Id를 확인해주세요."),
-    BAD_REQUEST_STUDENT("400-3","존재하지 않는 학번입니다."),
     BAD_REQUEST_FILE_NAME_COLUNM("400-11", "파일에 '이름' 칼럼을 확인해주세요."),
     BAD_REQUEST_FILE_STUDENT_ID_COLUNM("400-12", "파일에 '학번' 칼럼을 확인해주세요."),
     BAD_REQUEST_FILE_TYPE("400-13", "파일 형식이 맞지 않습니다.(엑셀, csv)"),
@@ -14,6 +11,9 @@ enum class ErrorCode (val code: String = "", val message: String = ""){
     FORBIDDEN_ACCESS("403", "Forbidden."),
 
     NOT_FOUND("404", "Not found."),
+    BAD_REQUEST_BAND("404-1", "밴드Id를 확인해주세요."),
+    BAD_REQUEST_EVENT("404-2", "이벤트Id를 확인해주세요."),
+    BAD_REQUEST_STUDENT("404-3","학번을 확인해주세요."),
 
     METHOD_NOT_ALLOWED("405", "Not allowed method."),
 
