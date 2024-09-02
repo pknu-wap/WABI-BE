7d578a33f32559541019ef19bed9b9c2f5d0a9a6
JongKyeong Kim <oo7bangjk@naver.com>
2024-09-03 01:15:25 +0900

    refact: List에 var을 val로 수정

--- wabi/src/main/kotlin/com/wap/wabi/band/service/BandEnrollService.kt
+++ wabi/src/main/kotlin/com/wap/wabi/band/service/BandEnrollService.kt
@@ -26,7 +26,7 @@ class BandEnrollService(
 	fun enrollBandStudent(bandId : Long, request : EnrollRequest) : Long {
 		val band = bandRepository.findById(bandId).orElseThrow { throw RestApiException(ErrorCode.NOT_FOUND_BAND) }
 
-		var bandStudents : MutableList<BandStudent> = mutableListOf()
+		val bandStudents : MutableList<BandStudent> = mutableListOf()
 		request.bandStudentDtos.map { bandStudentDto ->
 			val studentId = bandStudentDto.studentId
 			val studentName = bandStudentDto.name
