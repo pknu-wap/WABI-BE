426eb693044d8b13981117ec59679d4d892c05dd
FhRh <yiyop@naver.com>
2024-09-04 00:01:22 +0900

    feat : 엔티티마다 builder 패턴 적용

--- wabi/src/main/kotlin/com/wap/wabi/event/entity/Event.java
+++ wabi/src/main/kotlin/com/wap/wabi/event/entity/Event.java
@@ -15,6 +15,47 @@ public class Event {
     private String name;
     private LocalDateTime startAt;
 
+    private Event(builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.startAt = builder.startAt;
+        this.endAt = builder.endAt;
+    }
+
+    public static class builder {
+        private Long id;
+        private String name;
+        private LocalDateTime startAt;
+        private LocalDateTime endAt;
+
+        public builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public builder setStartAt(LocalDateTime startAt) {
+            this.startAt = startAt;
+            return this;
+        }
+
+        public builder setEndAt(LocalDateTime endAt) {
+            this.endAt = endAt;
+            return this;
+        }
+
+        public Event build() {
+            return new Event(this);
+        }
+    }
+
+    protected Event() {
+    }
+
     public Long getId() {
         return id;
     }
--- wabi/src/main/kotlin/com/wap/wabi/event/entity/EventBand.java
+++ wabi/src/main/kotlin/com/wap/wabi/event/entity/EventBand.java
@@ -18,6 +18,40 @@ public class EventBand {
     @ManyToOne(fetch = FetchType.LAZY)
     private Band band;
 
+    private EventBand(builder builder) {
+        this.id = builder.id;
+        this.event = builder.event;
+        this.band = builder.band;
+    }
+
+    public static class builder {
+        private Long id;
+        private Event event;
+        private Band band;
+
+        public builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public builder setEvent(Event event) {
+            this.event = event;
+            return this;
+        }
+
+        public builder setBand(Band band) {
+            this.band = band;
+            return this;
+        }
+
+        public EventBand build() {
+            return new EventBand(this);
+        }
+    }
+
+    protected EventBand() {
+    }
+
     public Long getId() {
         return id;
     }
