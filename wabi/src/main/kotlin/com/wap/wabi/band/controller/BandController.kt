7ae305631e6a17acefed08f2e850527c3d37437f
JongKyeong Kim <oo7bangjk@naver.com>
2024-09-06 14:29:29 +0900

    feat: POST/api/bands/create 추가

--- wabi/src/main/kotlin/com/wap/wabi/band/controller/BandController.kt
+++ wabi/src/main/kotlin/com/wap/wabi/band/controller/BandController.kt
@@ -1,5 +1,6 @@
 package com.wap.wabi.band.controller
 
+import com.wap.wabi.band.payload.request.BandCreateRequest
 import com.wap.wabi.band.service.BandService
 import com.wap.wabi.common.payload.response.Response
 import org.springframework.http.HttpStatus
@@ -7,7 +8,10 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/api/bands/")
@@ -17,6 +21,14 @@ class BandController(
     @GetMapping("{bandId}/students")
     fun getBandStudents(@PathVariable bandId: Long): ResponseEntity<Response> {
         val response = Response.ok(data = bandService.getBandStudents(bandId = bandId))
+        return ResponseEntity(response, HttpStatus.OK)
+    }
+
+    @PostMapping("create")
+    fun createBand(@RequestParam adminId: Long, @RequestBody request: BandCreateRequest): ResponseEntity<Response> {
+        bandService.createBand(adminId = adminId, bandCreateRequest = request)
+
+        val response = Response.ok(message = "success create band")
         return ResponseEntity(response, HttpStatus.OK)
     }
 }
