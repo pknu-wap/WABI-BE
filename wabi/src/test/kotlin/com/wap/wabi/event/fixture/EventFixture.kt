f32b4f2782604814cb4462c9148c1ae48ba45dd3
FhRh <yiyop@naver.com>
2024-09-06 11:43:10 +0900

    fix : event의 startAt과 endAt은 null일 수 없다

--- wabi/src/test/kotlin/com/wap/wabi/event/fixture/EventFixture.kt
+++ wabi/src/test/kotlin/com/wap/wabi/event/fixture/EventFixture.kt
@@ -2,14 +2,15 @@ package com.wap.wabi.event.fixture
 
 import com.wap.wabi.common.Reflection
 import com.wap.wabi.event.entity.Event
+import java.time.LocalDateTime
 
 object EventFixture {
     fun createEvent(name: String): Event {
         return Event.builder()
             .adminId(1)
             .name(name)
-            .startAt(null)
-            .endAt(null)
+            .startAt(LocalDateTime.now())
+            .endAt(LocalDateTime.now().plusDays(1))
             .eventStudentMaxCount(0)
             .build()
     }
