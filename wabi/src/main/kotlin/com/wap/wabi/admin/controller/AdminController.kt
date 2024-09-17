5fe995fc999a1b0788e803d8c999ece033a0c704
Zepelown <dbswjdqhd12@gmail.com>
2024-09-17 14:53:08 +0900

    fix: AdminController post 매핑으로 수정 - 기존 get 매핑에서 post 매핑으로 수정 issue #6

--- wabi/src/main/kotlin/com/wap/wabi/admin/controller/AdminController.kt
+++ wabi/src/main/kotlin/com/wap/wabi/admin/controller/AdminController.kt
@@ -5,9 +5,9 @@ import com.wap.wabi.admin.service.AdminService
 import com.wap.wabi.common.payload.response.Response
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,9 +15,10 @@ import org.springframework.web.bind.annotation.RestController
 class AdminController(
     private val adminService: AdminService
 ) {
-    @GetMapping("/register")
-    fun registerAdmin(@RequestParam("adminData") request: AdminRegisterRequest): ResponseEntity<Response> {
-        val response = Response.ok(data = adminService.registerAdmin(request))
+    @PostMapping("register")
+    fun registerAdmin(@RequestBody request: AdminRegisterRequest): ResponseEntity<Response> {
+        adminService.registerAdmin(request)
+        val response = Response.ok(message = "success register admin")
         return ResponseEntity(response, HttpStatus.OK)
     }
 }
