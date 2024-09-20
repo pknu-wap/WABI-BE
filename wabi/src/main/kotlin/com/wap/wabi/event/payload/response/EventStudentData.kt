60f9c5c36de1d8814a650db1577a39df3e0334a5
FhRh <yiyop@naver.com>
2024-09-20 18:57:59 +0900

    feat : 체크인 명단을 조회할 때, 중복등록되어 있는 사람은 하나로 표현

--- wabi/src/main/kotlin/com/wap/wabi/event/payload/response/EventStudentData.kt
+++ wabi/src/main/kotlin/com/wap/wabi/event/payload/response/EventStudentData.kt
@@ -2,29 +2,28 @@ package com.wap.wabi.event.payload.response
 
 import com.wap.wabi.event.entity.Enum.EventStudentStatus
 import com.wap.wabi.event.entity.EventStudent
+import com.wap.wabi.event.entity.EventStudentBandName
 import java.time.LocalDateTime
 
 data class EventStudentData(
     val id: String,
     val name: String,
-    val band: String,
     val eventStudentStatus: EventStudentStatus,
-    val checkInTime: LocalDateTime?
+    val checkInTime: LocalDateTime?,
+    val bandName: String
 ) {
     companion object {
-        fun of(eventStudents: List<EventStudent>): List<EventStudentData> {
-            return eventStudents.map {
-                of(it)
+        fun of(eventStudent: EventStudent, bandNames: List<EventStudentBandName>): EventStudentData {
+            var totalBandName = ""
+            bandNames.map { bandName ->
+                totalBandName = totalBandName + ", " + bandName.bandName
             }
-        }
-
-        fun of(eventStudent: EventStudent): EventStudentData {
             return EventStudentData(
                 eventStudent.student.id,
                 eventStudent.student.name,
-                eventStudent.band.bandName,
                 eventStudent.status,
-                eventStudent.checkedInAt
+                eventStudent.checkedInAt,
+                totalBandName
             )
         }
     }
