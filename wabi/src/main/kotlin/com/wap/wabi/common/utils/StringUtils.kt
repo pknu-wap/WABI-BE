d1682127642e0e54b5b0c345b9c400aac34d5c65
Zepelown <dbswjdqhd12@gmail.com>
2024-09-20 17:19:14 +0900

    refactor: StringUtils의 사용하지 않는 어노테이션 삭제 issue #6

--- wabi/src/main/kotlin/com/wap/wabi/common/utils/StringUtils.kt
+++ wabi/src/main/kotlin/com/wap/wabi/common/utils/StringUtils.kt
@@ -4,19 +4,16 @@ import java.util.regex.Pattern
 
 class StringUtils {
     companion object {
-        @JvmStatic
         fun checkLength(string: String, start: Int, end: Int): Boolean {
             val length = string.length
             return length in start..end
         }
 
-        @JvmStatic
         fun hasOnlySmallLetterOrNumber(string: String): Boolean {
             val pattern = "^[a-z0-9]*$".toRegex()
             return pattern.matches(string)
         }
 
-        @JvmStatic
         fun hasOnlyAllowedSpecialCharacters(input: String, specialCharacters: String): Boolean {
             val allowedPattern = "^[a-zA-Z0-9${Pattern.quote(specialCharacters)}]*$".toRegex()
 
