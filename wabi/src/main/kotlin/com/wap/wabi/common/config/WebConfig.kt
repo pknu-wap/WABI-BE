bac4b7f529eba6865060e78e8e73d3afc1636791
FhRh <yiyop@naver.com>
2024-09-20 19:10:21 +0900

    feat : CORS 허용

--- wabi/src/main/kotlin/com/wap/wabi/common/config/WebConfig.kt
+++ wabi/src/main/kotlin/com/wap/wabi/common/config/WebConfig.kt
@@ -5,11 +5,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer{
+class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
             .allowCredentials(true);
     }
 }
