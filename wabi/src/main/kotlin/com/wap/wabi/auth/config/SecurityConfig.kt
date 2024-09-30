58bd55052799440a5c2c7a363ded8b6ae8bbce6a
Zepelown <dbswjdqhd12@gmail.com>
2024-09-30 13:04:24 +0900

    refactor: 불필요 import 삭제 issue #6

--- wabi/src/main/kotlin/com/wap/wabi/auth/config/SecurityConfig.kt
+++ wabi/src/main/kotlin/com/wap/wabi/auth/config/SecurityConfig.kt
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
