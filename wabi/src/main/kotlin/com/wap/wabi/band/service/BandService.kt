ff5bc31c131f95ca47b9f3edc8f96e757e54433f
JongKyeong Kim <oo7bangjk@naver.com>
2024-10-08 16:14:02 +0900

    feat: 밴드 삭제 시 종속성 가진 테이블 같이 삭제

--- wabi/src/main/kotlin/com/wap/wabi/band/service/BandService.kt
+++ wabi/src/main/kotlin/com/wap/wabi/band/service/BandService.kt
@@ -1,5 +1,6 @@
 package com.wap.wabi.band.service
 
+import com.wap.wabi.auth.admin.repository.AdminRepository
 import com.wap.wabi.band.entity.Band
 import com.wap.wabi.band.payload.request.BandCreateRequest
 import com.wap.wabi.band.payload.request.BandUpdateRequest
@@ -8,6 +9,11 @@ import com.wap.wabi.band.payload.response.BandStudentData
 import com.wap.wabi.band.payload.response.BandsData
 import com.wap.wabi.band.repository.BandRepository
 import com.wap.wabi.band.repository.BandStudentRepository
+import com.wap.wabi.event.entity.Event
+import com.wap.wabi.event.repository.EventBandRepository
+import com.wap.wabi.event.repository.EventRepository
+import com.wap.wabi.event.repository.EventStudentBandNameRepository
+import com.wap.wabi.event.repository.EventStudentRepository
 import com.wap.wabi.exception.ErrorCode
 import com.wap.wabi.exception.RestApiException
 import jakarta.transaction.Transactional
@@ -15,7 +21,13 @@ import org.springframework.stereotype.Service
 
 @Service
 class BandService(
-    private val bandRepository: BandRepository, private val bandStudentRepository: BandStudentRepository
+    private val bandRepository: BandRepository,
+    private val bandStudentRepository: BandStudentRepository,
+    private val adminRepository: AdminRepository,
+    private val eventStudentBandNameRepository: EventStudentBandNameRepository,
+    private val eventBandRepository: EventBandRepository,
+    private val eventRepository: EventRepository,
+    private val eventStudentRepository: EventStudentRepository
 ) {
     @Transactional
     fun getBandStudents(bandId: Long): List<BandStudentData> {
@@ -28,11 +40,13 @@ class BandService(
 
     @Transactional
     fun createBand(adminId: Long, bandCreateRequest: BandCreateRequest) {
-        if (adminId != TEMPORARY_ADMIN_ID) {
+        val admin = adminRepository.findById(adminId)
+
+        if (admin.isEmpty) {
             throw RestApiException(ErrorCode.UNAUTHORIZED_REQUEST)
         }
 
-        val createBand = bandCreateRequest.toBand(adminId)
+        val createBand = bandCreateRequest.toBand(admin.get().id)
 
         bandRepository.save(createBand)
     }
@@ -40,8 +54,9 @@ class BandService(
     @Transactional
     fun deleteBand(adminId: Long, bandId: Long) {
         val band = bandRepository.findById(bandId).orElseThrow { RestApiException(ErrorCode.NOT_FOUND_BAND) }
+        val admin = adminRepository.findById(adminId)
 
-        if (adminId != TEMPORARY_ADMIN_ID) {
+        if (admin.isEmpty) {
             throw RestApiException(ErrorCode.UNAUTHORIZED_REQUEST)
         }
 
@@ -49,20 +64,39 @@ class BandService(
             throw RestApiException(ErrorCode.ALREADY_ADD_STUDENT)
         }
 
-        if (band.adminId != adminId) {
+        if (band.adminId != admin.get().id) {
             throw RestApiException(ErrorCode.UNAUTHORIZED_BAND)
         }
 
+        val events = eventRepository.findAllByBand(band)
+        events.forEach { event ->
+            deleteEventByBand(event = event)
+        }
+        bandStudentRepository.deleteAllByBand(band)
         bandRepository.delete(band)
+    }
+
+    private fun deleteEventByBand(event: Event) {
+        val eventStudens = eventStudentRepository.findAllByEvent(event)
+
+        eventStudens.forEach { eventStudent ->
+            eventStudentBandNameRepository.deleteByEventStudent(eventStudent)
+        }
+
+        eventStudentRepository.deleteByEvent(event)
+        eventBandRepository.deleteByEvent(event)
+        eventRepository.delete(event)
     }
 
     @Transactional
     fun getBands(adminId: Long): List<BandsData> {
-        if (adminId != TEMPORARY_ADMIN_ID) {
+        val admin = adminRepository.findById(adminId)
+
+        if (admin.isEmpty) {
             throw RestApiException(ErrorCode.UNAUTHORIZED_REQUEST)
         }
 
-        val bands: List<Band> = bandRepository.findAllByAdminId(adminId)
+        val bands: List<Band> = bandRepository.findAllByAdminId(admin.get().id)
         val bandsDatas: MutableList<BandsData> = mutableListOf()
         bands.forEach { band ->
             bandsDatas.add(BandsData(bandId = band.id, bandName = band.bandName))
@@ -75,12 +109,13 @@ class BandService(
     fun updateBand(adminId: Long, bandUpdateRequest: BandUpdateRequest) {
         val band = bandRepository.findById(bandUpdateRequest.bandId)
             .orElseThrow { RestApiException(ErrorCode.NOT_FOUND_BAND) }
+        val admin = adminRepository.findById(adminId)
 
-        if (adminId != TEMPORARY_ADMIN_ID) {
+        if (admin.isEmpty) {
             throw RestApiException(ErrorCode.UNAUTHORIZED_REQUEST)
         }
 
-        if (band.adminId != adminId) {
+        if (band.adminId != admin.get().id) {
             throw RestApiException(ErrorCode.UNAUTHORIZED_BAND)
         }
 
@@ -93,9 +128,5 @@ class BandService(
             .orElseThrow { RestApiException(ErrorCode.NOT_FOUND_BAND) }
 
         return BandDetailData.of(band = band)
-    }
-
-    companion object {
-        private const val TEMPORARY_ADMIN_ID = 1L
     }
 }
--- wabi/src/main/kotlin/com/wap/wabi/event/repository/EventRepository.java
+++ wabi/src/main/kotlin/com/wap/wabi/event/repository/EventRepository.java
@@ -1,5 +1,6 @@
 package com.wap.wabi.event.repository;
 
+import com.wap.wabi.band.entity.Band;
 import com.wap.wabi.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByAdminId(Long id);
+
+    List<Event> findAllByBand(Band band);
 }
